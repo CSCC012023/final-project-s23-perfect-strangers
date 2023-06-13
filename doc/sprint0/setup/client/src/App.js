c6eb670cc829b477022c4aa9720e359923a1a7d6
Fahrin0802 <farhan.faisal@mail.utoronto.ca>
2023-06-13 15:36:37 -0400

    develoted app.js scrap code

--- doc/sprint0/setup/client/src/App.js
+++ doc/sprint0/setup/client/src/App.js
@@ -36,44 +36,6 @@ function App() {
   return (
     <div className="App">
       <BioPage/>
-
-      {/* <div className='usersDisplay'>
-        {listOfUsers.map((user) => {
-          return (
-          <div>
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <h1>Username: {user.username}</h1>
-            <hr/>
-          </div>)
-        })
-        }
-      </div>
-      
-       <div>
-        <input 
-        type='text' 
-        placeholder='Name' 
-        onChange={(event) => {
-          setName(event.target.value);
-          }}
-          />
-
-        <input 
-        type='number' 
-        placeholder='Age'
-        onChange={(event) => {
-          setAge(event.target.value);
-          }}/>
-
-        <input 
-        type='text' 
-        placeholder='Username'
-        onChange={(event) => {
-          setUsername(event.target.value);
-          }}/>
-        <button onClick={createUser}> Create User</button>
-      </div> */}
     </div>
   );
 }
