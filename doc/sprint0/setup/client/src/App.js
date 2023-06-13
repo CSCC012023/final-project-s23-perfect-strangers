3fad9a5ce450570d61717ba09b1ead9a7619b705
AthulVincent <athulvincent2019@gmail.com>
2023-06-13 15:56:34 -0400

    Resolve conflict

--- doc/sprint0/setup/client/src/App.js
+++ doc/sprint0/setup/client/src/App.js
@@ -1,41 +1,11 @@
+import UserBio from "./components/UserBio";
 import './App.css';
-import { useState, useEffect } from "react";
-import Axios from "axios";
-import BioPage from "./BioPage";
 import UserBio from "./UserBio";
 
 function App() {
-  const [listOfUsers, setListOfUsers] = useState([]);
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    Axios.get("http://localhost:5000/api/users").then((response) => {
-      setListOfUsers(response.data);
-    });
-    console.log(listOfUsers);
-  }, []);
-
-
-  const createUser = () => {
-    Axios.post("http://localhost:5000/api/users/", {
-      name: name, 
-      age: age, 
-      username: username,
-    }).then((response) => {
-      setListOfUsers([...listOfUsers, {
-        name: name, 
-        age: age, 
-        username: username,
-      }])
-      alert("User Created!");
-    });
-  };
-
   return (
     <div className="App">
-      <BioPage/>
+      <UserBio />
     </div>
   );
 }
--- doc/sprint0/setup/client/src/index.js
+++ doc/sprint0/setup/client/src/index.js
@@ -4,12 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"; // npm install --save react-bootstrap bootstrap@3
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
