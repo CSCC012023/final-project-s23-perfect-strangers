4e6acc060ac01ec2a0b2cbf63643f6abfb672cb3
AthulVincent <90774897+AthulVincent@users.noreply.github.com>
2023-06-14 20:45:24 -0400

    About Me page (#13)
    * Working standalone About Me page
    
    * Minor Changes
    
    * BioPage draft -> Interest + Biography (#11)
    
    * Implemented interest-edit feature frontend
    
    * Removed temp files
    
    * Implemented interest feature backend
    
    * made skeleton of metaAuth
    
    * -Draft merge with athul
    
    * develoted app.js scrap code
    
    * added server gitignore
    
    * Resolve conflict
    
    * Touch up
    
    * Touch up
    
    * Change colours
    
    * Minor change
    
    ---------
    
    Co-authored-by: Farhan Bin Faisal <77621905+Farhan-Faisal@users.noreply.github.com>

--- doc/sprint0/setup/client/src/index.js
+++ doc/sprint0/setup/client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"; // npm install --save react-bootstrap bootstrap@3
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
