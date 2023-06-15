89176669eca8cd4d53b8be0ba5555c2057bbd4ae
AthulVincent <90774897+AthulVincent@users.noreply.github.com>
2023-06-14 21:27:40 -0400

    Revert "About Me page (#13)"
    This reverts commit 4e6acc060ac01ec2a0b2cbf63643f6abfb672cb3.

--- doc/sprint0/setup/client/src/index.js
+++ doc/sprint0/setup/client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css"; // npm install --save react-bootstrap bootstrap@3
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- doc/sprint0/setup/server/models/usersModel.js
+++ doc/sprint0/setup/server/models/usersModel.js
@@ -1,25 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 2,
-  },
-  biography: {
-    type: String,
-    trim: true,
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 2
+    }
 });
 
 const UserModel = mongoose.model("users", userSchema);
