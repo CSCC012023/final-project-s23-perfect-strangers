e926ebf0966d06f3a31862cde3a52eca6d41fe04
BVChamarthi <bharathvarma169@gmail.com>
2023-06-29 21:39:36 -0400

    DEV-CGP-10: added documentation for request.routes.js

--- doc/sprint0/setup/server/routes/request.routes.js
+++ doc/sprint0/setup/server/routes/request.routes.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 let RequestModel = require('../models/request.model');
 
 // POST REQUEST: create a request
+/* 
+    //example usage
+
+    Axios.post("http://localhost:5000/requests/", {
+        requester: requester@mail.com
+        requestee: requestee@mail.com
+        event_id: rand_event_id
+    })
+    .then(res => {
+        // do stuff...
+    })
+*/
 router.route('/').post(
     (req, res) => {
         const newRequest = new RequestModel({
@@ -19,7 +31,17 @@ router.route('/').post(
     }
 )
 
+
+
 // GET REQUEST: get a list of requests by who issued them
+/* 
+    //example usage
+
+    Axios.get("http://localhost:5000/requests/by/requester@mail.com", {})
+    .then(res => {
+        // do stuff...
+    })
+*/
 router.route('/by/:requester').get(
     (req, res) => {
         RequestModel.find({requester: req.params.requester})
@@ -28,7 +50,17 @@ router.route('/by/:requester').get(
     }
 )
 
+
+
 // GET REQUEST: get a list of requests by who received them
+/* 
+    //example usage
+
+    Axios.get("http://localhost:5000/requests/for/requestee@mail.com", {})
+    .then(res => {
+        // do stuff...
+    })
+*/
 router.route('/for/:requestee').get(
     (req, res) => {
         RequestModel.find({requestee: req.params.requestee})
@@ -37,7 +69,17 @@ router.route('/for/:requestee').get(
     }
 )
 
+
+
 // GET REQUEST: get a list of requests by event_id
+/* 
+    //example usage
+
+    Axios.get("http://localhost:5000/requests/event/rand_event_id", {})
+    .then(res => {
+        // do stuff...
+    })
+*/
 router.route('/event/:event_id').get(
     (req, res) => {
         RequestModel.find({event_id: req.params.event_id})
@@ -46,10 +88,31 @@ router.route('/event/:event_id').get(
     }
 )
 
+
+
 // GET REQUEST: get a list of requests based on a filter passed in the body
+/* 
+    //example usage
+
+    Axios.get("http://localhost:5000/requests/search", {
+        requester: requester@mail.com
+        requestee: requestss@mail.com
+        event_id: rand_email_id
+    }).then(res => {
+        // do stuff...
+    })
+
+    // all filters are optional, here's another example
+
+    Axios.get("http://localhost:5000/requests/search", {
+        requester: requester@mail.com
+    }).then(res => {
+        // do stuff...
+    })
+*/
 router.route('/search').get(
     (req, res) => {
-        RequestModel.find(req.body.filter)
+        RequestModel.find(req.body)
         .then(r => res.json(r))
         .catch(err => res.json({err: err}))
     }
