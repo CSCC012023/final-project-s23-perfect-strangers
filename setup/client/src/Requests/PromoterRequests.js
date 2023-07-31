281cf01eaa654db4fc954536680b43c0bf6fca6c
AthulVincent <athulvincent2019@gmail.com>
2023-07-31 16:52:53 -0400

    UI changes

--- setup/client/src/Requests/PromoterRequests.js
+++ setup/client/src/Requests/PromoterRequests.js
@@ -82,28 +82,57 @@ const PromoterRequestsPage = () => {
         >
           Send Request
         </button>
+        <div className={requestStyles.popupHeading2}>Promoters: </div>
         <ul className={requestStyles.unorderedList}>
-          {requestData.map((req) => (
-            <li className={requestSentStyles.requestSentCard}>
-              <img
-                src={`http://localhost:5000/uploads/` + req.requestee.image}
-                alt="No photo"
-                className={requestSentStyles.eventPhoto}
-              />
-              <div className={requestSentStyles.requestSentCardContent}>
-                <h4>
-                  <b>{req.requestee.username}</b>
-                </h4>
-                <p>{req.requestee.age}</p>
-                <p>{makeFirstLetterCapital(req.requestee.gender)}</p>
-                <p>{req.requestee.email}</p>
-                {req.requestee.biography && <p>{req.requestee.biography}</p>}
-              </div>
-              <div className={requestSentStyles.requestCardStatus}>
-                {makeFirstLetterCapital(req.status)}
-              </div>
-            </li>
-          ))}
+          {requestData
+            .filter((req) => req.status === "accepted")
+            .map((req) => (
+              <li className={requestSentStyles.requestSentCard}>
+                <img
+                  src={`http://localhost:5000/uploads/` + req.requestee.image}
+                  alt="No photo"
+                  className={requestSentStyles.eventPhoto}
+                />
+                <div className={requestSentStyles.requestSentCardContent}>
+                  <h4>
+                    <b>{req.requestee.username}</b>
+                  </h4>
+                  <p>{req.requestee.age}</p>
+                  <p>{makeFirstLetterCapital(req.requestee.gender)}</p>
+                  <p>{req.requestee.email}</p>
+                </div>
+                <div className={requestSentStyles.promoterAcceptedInvites}>
+                  <text>Number of accepted invites:</text>
+                  <p>1</p>
+                </div>
+              </li>
+            ))}
+        </ul>
+        <div className={requestStyles.popupHeading2}>Pending Requests: </div>
+        <ul className={requestStyles.unorderedList}>
+          {requestData
+            .filter((req) => req.status === "pending")
+            .map((req) => (
+              <li className={requestSentStyles.requestSentCard}>
+                <img
+                  src={`http://localhost:5000/uploads/` + req.requestee.image}
+                  alt="No photo"
+                  className={requestSentStyles.eventPhoto}
+                />
+                <div className={requestSentStyles.requestSentCardContent}>
+                  <h4>
+                    <b>{req.requestee.username}</b>
+                  </h4>
+                  <p>{req.requestee.age}</p>
+                  <p>{makeFirstLetterCapital(req.requestee.gender)}</p>
+                  <p>{req.requestee.email}</p>
+                  {req.requestee.biography && <p>{req.requestee.biography}</p>}
+                </div>
+                <div className={requestSentStyles.requestCardStatus}>
+                  {makeFirstLetterCapital(req.status)}
+                </div>
+              </li>
+            ))}
         </ul>
       </>
     );
